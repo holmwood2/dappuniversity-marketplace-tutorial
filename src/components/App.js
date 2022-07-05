2a3072dad77b3d01c3b468d3ebae4ab655f64821
Holmwood2 <104306241+holmwood2@users.noreply.github.com>
2022-07-05 13:30:10 +0100

    Add navbar + remove web3 deprecated dep

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar'
-import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
 import Marketplace from '../abis/Marketplace.json'
 import logo from '../logo.png';
 import './App.css';
 
+
 class App extends Component {
+
 	async componentWillMount() {
-		await this.loadWeb3()
-    await this.loadBlockchainData()
-	}
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      this.loadBlockchainData(provider)
     } else {
-      window.alert('non ethereum browser detected')
+      window.alert('Ethereum provider not found - please install metamask')
     }
-  }
-  async loadBlockchainData() {
-    const web3 = window.web3
-    const accounts = await web3.eth.getAccounts()
+	}
+  async loadBlockchainData(provider) {
+    const accounts = await provider.request({ method: 'eth_accounts' })
     console.log(accounts)
     this.setState({account: accounts[0]})
-    const networkId = await web3.eth.net.getId()
+    // const networkId = await web3.eth.net.getId()
+    const networkId = await provider.request({ method: 'net_version' })
     const networkData = Marketplace.networks[networkId]
     if (networkData) {
-      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      console.log(marketplace)
+      console.log("marketplace address %s", networkData.address)
     } else {
       window.alert('Marketplace contract not deployed')
     }
